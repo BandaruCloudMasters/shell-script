a403de0bb35cb515abdb70ac114dd8d28eaa7982
Suresh Bandaru <talktosureshbandaru@gmail.com>
2025-07-05 13:25:39 +0530

    Added conversation

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,6 +4,6 @@ PERSON1=$1 #no space before and after equal to
 PERSON2=$2
  # sh 04-variables.sh John Sardar
 echo "$PERSON1:: Hi $PERSON2"
-echo "{$PERSON2}:: Hello $PERSON1"
+echo "${PERSON2}:: Hello $PERSON1"
 echo "$PERSON1:: How are you doing"
 echo "$PERSON2:: I am good. How are you?"
