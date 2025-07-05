5e0fb932e97a5ded3d40b1d06f9725cfd1e1c403
Suresh Bandaru <talktosureshbandaru@gmail.com>
2025-07-05 13:24:23 +0530

    Added conversation

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PERSON1=Ramesh #no space before and after equal to
-PERSON2=Suresh
-
+PERSON1=$1 #no space before and after equal to
+PERSON2=$2
+ # sh 04-variables.sh John Sardar
 echo "$PERSON1:: Hi $PERSON2"
 echo "{$PERSON2}:: Hello $PERSON1"
 echo "$PERSON1:: How are you doing"
